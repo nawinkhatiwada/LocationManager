090013928396bdd7d835e89d31e6230f0b3cfd43
nawinkhatiwada <nawin.khatiwada@gmail.com>
2019-12-31 08:19:48 +0545

    increased timeout to 20 sec

--- library/src/main/java/com/androidbolts/library/utils/LocationConstants.kt
+++ library/src/main/java/com/androidbolts/library/utils/LocationConstants.kt
@@ -3,7 +3,7 @@ package com.androidbolts.library.utils
 object LocationConstants {
     const val TIME_OUT_NONE = 0L
     const val TIME_OUT_SHORT = 10000L
-    const val TIME_OUT_LONG = 15000L
+    const val TIME_OUT_LONG = 20000L
     const val LOCATION_PERMISSIONS_REQUEST_CODE = 1
     const val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
     const val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
